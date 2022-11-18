1e4820e949facd43acc4c101ae3070089f8a4f0f
Parimala Jangam <parimala.jangam@ATMECS.CORP>
2022-11-18 12:23:14 +0530

    commit the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstApp';
+ // title = 'FirstApp';
+
+password = '';
+
+
+onButtonClick(){
+this.password = "my password";
+  }
 }
